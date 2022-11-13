3937719f3f7df24646c2bfac11154b36e72588a9
masha.kostryukova.98@mail.ru <masha.kostryukova.98@mail.ru>
2022-11-13 18:02:56 +0300

    Homework3 is done

--- src/Homework3.java
+++ src/Homework3.java
@@ -5,6 +5,7 @@ public class Homework3 {
     public static void main(String[] args) {
 
         int correctCount = 0, wrongCount = 0;
+        final int NUMBER_ANSWER_OPTIONS = 4;
 
         String[] questions = {
                 "В файл с каким расширением компилируется java-файл",
@@ -15,12 +16,12 @@ public class Homework3 {
         String[][] answerOptions = {
                 {"1. cs", "2. java", "3. class", "4. exe"},
                 {"1. commit", "2. push", "3. clone", "4. copy"},
-                {"1. while", "2. for", "3. loop"}
+                {"1. while", "2. for", "3. loop", "4. do"}
         };
 
         int[] correctAnswers = {3, 3, 1};
         Scanner scanner = new Scanner(System.in);
-
+        int answer = 0;
         for (int i = 0; i < questions.length; i++) {
 
             System.out.println(questions[i]);
@@ -30,7 +31,12 @@ public class Homework3 {
             }
 
             System.out.print("Ваш ответ: ");
-            int answer = scanner.nextInt();
+            answer = scanner.nextInt();
+            while (answer<1 || answer>NUMBER_ANSWER_OPTIONS) {
+                System.out.println("Введено некорректное число. Попробуйте еще раз.");
+                System.out.print("Ваш ответ: ");
+                answer = scanner.nextInt();
+            }
 
             if (answer == correctAnswers[i]) {
                 System.out.println("Правильно");
@@ -39,6 +45,7 @@ public class Homework3 {
                 System.out.println("Неправильно");
                 wrongCount++;
             }
+            answer = 0;
         }
 
         System.out.println("Результат: правильно " + correctCount + ", неправильно " + wrongCount);
